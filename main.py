6d2ef36e7a050d1bd729c32e7cfed83955583535
Manikandan R S <rsmanikandan.dev@gmail.com>
2023-03-09 18:37:58 +0530

    generate for 60 sec

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import io
 from PIL import Image, ImageDraw
 import cv2
 import random
+import logging
+from time import time
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def generate_image(color):
-    img = Image.new(mode = "1", size = (512, 512), color="white")
+    img = Image.new(mode = "1", size = (128, 128), color="white")
     draw = ImageDraw.Draw(img)
-    for x in range(512):
-        draw.point(xy = (random.randint(0,512), random.randint(0,512)), fill = "black")
+    for x in range(128):
+        for y in range(128):
+            if (random.randint(0,1) == 1):
+                draw.point(xy = (x,y), fill = "black")
 
     img.save(fp = "test.png")
     img_byte_arr = io.BytesIO()
@@ -17,36 +24,21 @@ def generate_image(color):
 def generate_video():
     video_name = 'mygeneratedvideo.avi'
 
-    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'MJPG'), 60.0, (512, 512))
+    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'MJPG'), 60.0, (128, 128))
     
-    for color in range(6):
-        if (color == 1):
-            for x in range(60):
-                generate_image("black")
-                image = cv2.imread("test.png")
-                video.write(image)
-        if (color == 2):
-            for x in range(60):
-                generate_image("yellow")
-                image = cv2.imread("test.png")
-                video.write(image)        
-        if (color == 3):
-            for x in range(60):
-                generate_image("green")
-                image = cv2.imread("test.png")
-                video.write(image)      
-        if (color == 4):
-            for x in range(60):
-                generate_image("blue")
-                image = cv2.imread("test.png")
-                video.write(image)   
-        if (color == 5):
-            for x in range(60):
-                generate_image("red")
-                image = cv2.imread("test.png")
-                video.write(image)
+    for color in range(60):
+        for x in range(60):
+            generate_image("black")
+            image = cv2.imread("test.png")
+            video.write(image)
 
     cv2.destroyAllWindows() 
     video.release()
 
-generate_video()
+def main():
+    ts = time()
+    generate_video()
+    logging.info('Took %s seconds', time() - ts)
+
+if __name__ == '__main__':
+    main()
