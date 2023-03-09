c5ad872de165b20e4ef90ed3d1c8361332144843
Manikandan R S <rsmanikandan.dev@gmail.com>
2023-03-09 20:14:35 +0530

    Convert Text file to image binart

--- main.py
+++ main.py
@@ -8,11 +8,35 @@ from time import time
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-def generate_image(color):
-    img = Image.new(mode = "1", size = (128, 128), color="white")
+def open_file():
+    in_file = open("text_file.txt", "rb") 
+    data = in_file.read()
+    byte_array = bytearray(data)
+    res = []
+    for byte in byte_array:
+        binary_rep = bin(byte)
+        res.append(binary_rep[2:])
+    generate_image_using_data(res)    
+    in_file.close()
+
+def generate_image_using_data(data):
+    img = Image.new(mode = "1", size = (512, 512), color="white")
     draw = ImageDraw.Draw(img)
-    for x in range(128):
-        for y in range(128):
+    for x in range(len(data)):
+        for y in range(len(data[x])):
+            if (data[x][y] == '1'):
+                draw.point(xy = (x,y), fill = "black")
+
+    img.save(fp = "test.png")
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()    
+
+def generate_image():
+    img = Image.new(mode = "1", size = (16, 16), color="white")
+    draw = ImageDraw.Draw(img)
+    for x in range(16):
+        for y in range(16):
             if (random.randint(0,1) == 1):
                 draw.point(xy = (x,y), fill = "black")
 
@@ -24,11 +48,11 @@ def generate_image(color):
 def generate_video():
     video_name = 'mygeneratedvideo.avi'
 
-    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'MJPG'), 60.0, (128, 128))
+    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'MJPG'), 60.0, (16, 16))
     
-    for color in range(60):
+    for color in range(1):
         for x in range(60):
-            generate_image("black")
+            generate_image()
             image = cv2.imread("test.png")
             video.write(image)
 
@@ -37,8 +61,10 @@ def generate_video():
 
 def main():
     ts = time()
-    generate_video()
+    #generate_video()
     logging.info('Took %s seconds', time() - ts)
+    open_file()
 
 if __name__ == '__main__':
     main()
+    
